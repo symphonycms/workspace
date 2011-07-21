f58edd6f5785666303325563dfc87620e852679a
Craig Zheng <craig.zheng@gmail.com>
2011-07-21 14:45:47 -0400

    2.2.2beta2 update

--- install.sql
+++ install.sql
@@ -283,8 +283,8 @@ CREATE TABLE `tbl_entries_data_3` (
 ) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- *** DATA: `tbl_entries_data_3` ***
-INSERT INTO `tbl_entries_data_3` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (12, 3, '2011-01-03T12:34:00+10:00', 1294079640, 1294079640);
-INSERT INTO `tbl_entries_data_3` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (11, 4, '2011-01-03T13:57:00+10:00', 1294084620, 1294084620);
+INSERT INTO `tbl_entries_data_3` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (12, 3, '2011-07-20T12:34:00+10:00', 1294079640, 1294079640);
+INSERT INTO `tbl_entries_data_3` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (11, 4, '2011-07-21T13:57:00+10:00', 1294084620, 1294084620);
 
 -- *** STRUCTURE: `tbl_entries_data_4` ***
 DROP TABLE IF EXISTS `tbl_entries_data_4`;
@@ -363,8 +363,8 @@ CREATE TABLE `tbl_entries_data_8` (
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- *** DATA: `tbl_entries_data_8` ***
-INSERT INTO `tbl_entries_data_8` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (4, 5, '2011-02-23T14:54:00+10:00', 1266900840, 1266900840);
-INSERT INTO `tbl_entries_data_8` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (3, 6, '2011-02-23T14:55:00+10:00', 1266900900, 1266900900);
+INSERT INTO `tbl_entries_data_8` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (4, 5, '2011-07-20T14:54:00+10:00', 1266900840, 1266900840);
+INSERT INTO `tbl_entries_data_8` (`id`, `entry_id`, `value`, `local`, `gmt`) VALUES (3, 6, '2011-07-21T14:55:00+10:00', 1266900900, 1266900900);
 
 -- *** STRUCTURE: `tbl_entries_data_9` ***
 DROP TABLE IF EXISTS `tbl_entries_data_9`;
@@ -383,24 +383,24 @@ INSERT INTO `tbl_entries_data_9` (`id`, `entry_id`, `value`, `value_formatted`) 
 INSERT INTO `tbl_entries_data_9` (`id`, `entry_id`, `value`, `value_formatted`) VALUES (3, 6, 'Follow the Symphony team ([@symphonycms](http://twitter.com/symphonycms)) and the Symphony community ([#symphonycms](http://twitter.com/search?q=%23symphonycms)) on Twitter.', '<p>Follow the Symphony team (<a href=\"http://twitter.com/symphonycms\">@symphonycms</a>) and the Symphony community (<a href=\"http://twitter.com/search?q=%23symphonycms\">#symphonycms</a>) on Twitter.</p>\n');
 
 -- *** DATA: `tbl_entries` ***
-INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (1, 2, 1, '2011-02-23 15:03:54', '2011-02-23 05:03:54');
-INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (2, 2, 1, '2011-02-23 15:05:01', '2011-02-23 05:05:01');
-INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (3, 1, 1, '2011-02-23 15:05:45', '2011-02-23 05:05:45');
-INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (4, 1, 1, '2011-02-23 15:06:00', '2011-02-23 05:06:00');
-INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (5, 3, 1, '2011-02-23 15:06:19', '2011-02-23 05:06:19');
-INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (6, 3, 1, '2011-02-23 15:06:31', '2011-02-23 05:06:31');
+INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (1, 2, 1, '2011-07-21 15:03:54', '2011-07-21 05:03:54');
+INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (2, 2, 1, '2011-07-21 15:05:01', '2011-07-21 05:05:01');
+INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (3, 1, 1, '2011-07-21 15:05:45', '2011-07-21 05:05:45');
+INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (4, 1, 1, '2011-07-21 15:06:00', '2011-07-21 05:06:00');
+INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (5, 3, 1, '2011-07-21 15:06:19', '2011-07-21 05:06:19');
+INSERT INTO `tbl_entries` (`id`, `section_id`, `author_id`, `creation_date`, `creation_date_gmt`) VALUES (6, 3, 1, '2011-07-21 15:06:31', '2011-07-21 05:06:31');
 
 
 -- *** DATA: `tbl_extensions` ***
 INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES
-(129, 'debugdevkit', 'enabled', '1.1'),
-(130, 'export_ensemble', 'enabled', '1.12'),
-(131, 'selectbox_link_field', 'enabled', '1.19'),
-(132, 'jit_image_manipulation', 'enabled', '1.09'),
+(129, 'debugdevkit', 'enabled', '1.2'),
+(130, 'export_ensemble', 'enabled', '1.16'),
+(131, 'selectbox_link_field', 'enabled', '1.20pre'),
+(132, 'jit_image_manipulation', 'enabled', '1.10'),
 (133, 'maintenance_mode', 'enabled', '1.4'),
 (134, 'profiledevkit', 'enabled', '1.0.4'),
 (135, 'markdown', 'enabled', '1.13'),
-(136, 'xssfilter', 'enabled', '1.0');
+(136, 'xssfilter', 'enabled', '1.1');
 
 
 -- *** DATA: `tbl_extensions_delegates` ***
