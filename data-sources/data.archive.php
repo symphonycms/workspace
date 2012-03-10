d6fddd5f051f08e6e3a39149c775e69a8bee6009
Allen Chang <me@allenchang.com>
2012-03-10 21:03:59 +0800

    Fixed archive data source which was causing SQL errors over date field filtering

--- data-sources/data.archive.php
+++ data-sources/data.archive.php
@@ -1,70 +1,79 @@
 <?php
 
 	require_once(TOOLKIT . '/class.datasource.php');
-	
+
 	Class datasourcearchive extends Datasource{
-		
+
 		public $dsParamROOTELEMENT = 'archive';
 		public $dsParamORDER = 'desc';
 		public $dsParamGROUP = '3';
+		public $dsParamPAGINATERESULTS = 'yes';
 		public $dsParamLIMIT = '100';
+		public $dsParamSTARTPAGE = '1';
 		public $dsParamREDIRECTONEMPTY = 'no';
 		public $dsParamSORT = 'system:id';
-		public $dsParamSTARTPAGE = '1';
 		public $dsParamASSOCIATEDENTRYCOUNTS = 'yes';
 		
+
 		public $dsParamFILTERS = array(
-				'3' => '{$year:$this-year}',
+				'3' => '{$year}',
 				'5' => 'yes',
 		);
 		
+
 		public $dsParamINCLUDEDELEMENTS = array(
 				'title',
 				'date'
 		);
+		
 
 		public function __construct(&$parent, $env=NULL, $process_params=true){
 			parent::__construct($parent, $env, $process_params);
 			$this->_dependencies = array();
 		}
-		
+
 		public function about(){
 			return array(
-					 'name' => 'Archive',
-					 'author' => array(
-							'name' => 'Symphony Team',
-							'website' => 'http://symphony-cms.com',
-							'email' => 'team@symphony-cms.com'),
-					 'version' => '1.0',
-					 'release-date' => '2010-05-04');
+				'name' => 'Archive',
+				'author' => array(
+					'name' => 'Symphony Team',
+					'website' => 'http://symphony-cms.com',
+					'email' => 'team@symphony-cms.com'),
+				'version' => 'Symphony 2.3',
+				'release-date' => '2012-03-10T12:58:57+00:00'
+			);
 		}
-		
+
 		public function getSource(){
 			return '1';
 		}
-		
+
 		public function allowEditorToParse(){
 			return true;
 		}
-		
+
 		public function grab(&$param_pool=NULL){
 			$result = new XMLElement($this->dsParamROOTELEMENT);
-				
+
 			try{
+				
 				include(TOOLKIT . '/data-sources/datasource.section.php');
 			}
 			catch(FrontendPageNotFoundException $e){
 				// Work around. This ensures the 404 page is displayed and
 				// is not picked up by the default catch() statement below
 				FrontendPageNotFoundExceptionHandler::render($e);
-			}			
+			}
 			catch(Exception $e){
 				$result->appendChild(new XMLElement('error', $e->getMessage()));
 				return $result;
-			}	
+			}
 
 			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
 			return $result;
 		}
-	}
 
+	}
